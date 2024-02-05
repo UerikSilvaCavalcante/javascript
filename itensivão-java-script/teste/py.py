fe79fbd41d003ac3ff5ae939fb32370930a7afae
UerikSilvaCavalcante <156016908+UerikSilvaCavalcante@users.noreply.github.com>
2024-02-05 15:14:31 -0300

    h

--- itensivão-java-script/teste/py.py
+++ itensivão-java-script/teste/py.py
@@ -1,6 +1,9 @@
-val = "*"
-cont = 5
-for c in range(0,6):
-    print( " "* cont ,val * c)
-    cont -= 1
-    
+def arvore(altura):
+    stars = 1
+    maxColumns = altura * 2 - 1
+    for linha in range(altura):
+        espacos = (maxColumns - stars) // 2
+        print(espacos * " " + stars * "*" + espacos  * " ")
+        stars += 2
+
+arvore(30)
