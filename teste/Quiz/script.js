c4acb54f3162b241d2d9203a13047a0eb397d7f9
UerikSilvaCavalcante <156016908+UerikSilvaCavalcante@users.noreply.github.com>
2024-02-21 22:35:09 -0300

    quase pronto

--- teste/Quiz/script.js
+++ teste/Quiz/script.js
@@ -3,8 +3,17 @@ let btn_1 = document.getElementById("btn_1");
 let btn_2 = document.getElementById("btn_2");
 let btn_3 = document.getElementById("btn_3");
 let btn_4 = document.getElementById("btn_4");
+let barra = document.getElementById("barra");
+const conteiner = document.querySelector(".conteiner");
+let popup = document.querySelector(".popupload")
+let cont = document.getElementById("cont")
+let num_quest = document.getElementById("number_question")
 
+let btn_iniciar = document.getElementById("yes")
 
+let content = document.getElementById("icontent");
+let title = document.getElementById("ititle");
+let iniciar = document.getElementById("init")
 
 const perguntas = [
     {pergunta :"Qual é o maior planeta do sistema solar?" , 
@@ -40,11 +49,12 @@ btn_4.innerText = perguntas[0].opcoes[3]
 
 question.innerHTML = perguntas[0].pergunta
 
-function gera_pergunta(num){
+function gera_pergunta(){
+    num_quest.textContent = num + 1
     question.innerHTML = perguntas[num].pergunta
 }
 
-function botoes(num){
+function botoes(){
     btn_1.innerText = perguntas[num].opcoes[0]
     btn_2.innerText = perguntas[num].opcoes[1]
     btn_3.innerText = perguntas[num].opcoes[2]
@@ -52,31 +62,95 @@ function botoes(num){
 
 }
 
+function showPopupError(){
+    conteiner.classList.add("errado")
+    setTimeout(()=> {
+        conteiner.classList.remove("errado")
+        gera_pergunta()
+        botoes()
+    }, 3000)
+  
+}
+
+function showPopupAcerto(){
+    conteiner.classList.add("right")
+    setTimeout(()=> {
+        conteiner.classList.remove("right")
+        gera_pergunta()
+        botoes()
+    }, 3000)
+   
+}
+
+let intervalo;
+let contador = 30
+let isTime = true
+
+function tempo_resp(){
+    
+    intervalo = setInterval(()=>{
+        if (isTime == true){
+            contador --
+            cont.innerText = formatTime(contador)
+            if(contador <= 0){
+                isTime = false
+                console.log(contador)
+                confere_resp()
+                return contador
+                
+            }
+        }
+    }, 1000)
+    console.log(contador)    
+}
+
+
+
+function formatTime(tempo){
+    return tempo < 10 ? `0${tempo}` : tempo;
+}
+
+let acertos = 0
+
+// tempo_resp()
+
 function confere_resp(resposta){
     correct = perguntas[num].correta
     console.log(correct)
-    if (resposta == correct){
-        if (num >= 4){
-            num = 0
-        } else {
-            num ++
-        }
-        console.log(num)
-        gera_pergunta(num)
-        botoes(num)
-        
-        console.log("certa")
+    isClose = true;
+    let tempo = tempo_resp()
+
+    if (num >= 4){
+        num = 0
+        console.log(acertos)
     } else {
-        console.log("Errada")
-        alert("Resposta Errada")
+        num ++
     }
+    if (tempo <= 0){
+        showPopupError()
+        contador = 30
+    } else {
+        if (resposta == correct){
+            acertos ++
+            console.log(num)
+            console.log("certa")
+            showPopupAcerto()
+        } else {
+           
+            showPopupError()
+            console.log(num)
+        }
+    }
+    
 }
 
+while (content.sty)
 
 
 console.log(btn_1.textContent)
 btn_1.addEventListener("click", function() {
     confere_resp(btn_1.textContent)
+
 });
 btn_2.addEventListener('click', function (){
     confere_resp(btn_2.textContent)
@@ -88,4 +162,10 @@ btn_4.addEventListener('click', function() {
     confere_resp(btn_4.textContent)
 });
 
+btn_iniciar.addEventListener("click", ()=>{
+    iniciar.style.display = 'none'
+    content.style.display = 'flex'
+    title.style.display = 'block'
+    tempo_resp()
+})
 
